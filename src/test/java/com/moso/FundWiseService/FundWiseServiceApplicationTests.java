81f345a14e39539be5c46a17a265b9792b3d4cb2
movse <movse@host.docker.internal>
2025-05-03 16:22:21 -0400

    Initial project creation

---
+++ src/test/java/com/moso/FundWiseService/FundWiseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moso.FundWiseService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundWiseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
