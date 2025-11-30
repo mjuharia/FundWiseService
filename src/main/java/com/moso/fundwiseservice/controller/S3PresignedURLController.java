a47d266be1c00a09cfa3d36dc2b251cb84da17d8
mjuharia <movses.juharian@gmail.com>
2025-11-29 19:44:27 -0500

    Additional chages for fundwise

--- src/main/java/com/moso/fundwiseservice/controller/S3PresignedURLController.java
+++ src/main/java/com/moso/fundwiseservice/controller/S3PresignedURLController.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -106,7 +107,7 @@ public class S3PresignedURLController {
 		    return response;
 	        
 	    }
-		
+		@CrossOrigin(origins = "http://localhost:4200")
 		@GetMapping("/listfiles")
 	    public List<S3FileInfo> listFiles() {
 	        ListObjectsV2Request listRequest = ListObjectsV2Request.builder()
